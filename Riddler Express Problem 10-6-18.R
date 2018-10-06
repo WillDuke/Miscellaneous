dd770ed4325161e6202af6ac9a58b45605ba1189
WillDuke <Will@WillIams-MacBook-Pro-3.local>
2018-10-06 17:50:01 -0400

    renamed variables for clarity

--- Riddler Express Problem 10-6-18.R
+++ Riddler Express Problem 10-6-18.R
@@ -1,20 +1,20 @@
 hits <- replicate(100, {
-  success <- replicate(1000, {
-    w <- sample(2:10) #our random set of 9 cards
-    k <- w[1] #the first card
-    w <- w[-which(w %in% k)] #take the first card out of the deck
-    while (length(w) > 0){ #repeat until 1 card remains
-     p <- w[1] #make p the next card
-     if ((length(w[w < k]) > length(w[w > k]) & p < k) ||
-        (length(w[w < k]) < length(w[w > k]) & p > k) ||
-        (length(w[w < k]) == length(w[w > k]) & p > k)){ #prediction logic
-      w <- w[-which(w %in% p)] #if prediction is right, "turn over" second card
-      k = p #treat second card as first, and repeat until no cards left
-   }
-   else {break} #leave loop if the prediction is wrong
-   }
-   length(w) == 0 #if we get through all of the cards, call it a win!
-   })
+    success <- replicate(1000, {
+        deck <- sample(2:10) #our random set of 9 cards
+        first <- deck[1] #the first card
+        deck <- deck[-which(deck %in% first)] #take the first card out of the deck
+      while (length(deck) > 0){ #repeat until no cards remain
+        nxt <- deck[1] #make p the next card
+      if ((length(deck[deck < first]) > length(deck[deck > first]) & nxt < first) ||
+        (length(deck[deck < first]) < length(deck[deck > first]) & nxt > first) ||
+        (length(deck[deck < first]) == length(deck[deck > first]) & nxt > first)){ #prediction logic
+          deck <- deck[-which(deck %in% nxt)] #if prediction is right, "turn over" second card
+          first = nxt #treat next card as first, and repeat until no cards left
+        }
+      else {break} #leave loop if the prediction is wrong
+      }
+   length(deck) == 0 #if we get through all of the cards, call it a win!
+    })
   mean(success) #average the successes
 })
 mean(hits) #find the probability
